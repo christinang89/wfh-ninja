37658d08e535ac1d7a2c984fee7b16e79bd3440e
Christina Ng <christinang89@gmail.com>
2015-05-15 16:20:45 -0700

    "set up get excuse route"

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import *
+from flask.json import JSONEncoder
+import simplejson as json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/excuses", methods = ['GET'])
+def getExcuses():
+    d = {"excuseIds": ["1","2"]} 
+    return jsonify(**d)
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
