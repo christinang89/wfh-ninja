24616a0bef261b85c27a27d004ce1ad2ac15ccf2
Christina Ng <christinang89@gmail.com>
2015-05-15 16:25:12 -0700

    set up get individual excuse route

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def getExcuses():
     d = {"excuseIds": ["1","2"]} 
     return jsonify(**d)
 
+@app.route("/excuses/<int:id>", methods = ['GET'])
+def getExcuse(id):
+    d = {1:{"excuse":"test"}, 2:{"excuse":"boo"}}
+    return jsonify(**d[id])
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
