646691d41fc58431d4dafea0aa5f3923ab0b99d3
Christina <christinang89@gmail.com>
2015-05-23 17:33:28 -0700

    add login functionality

--- main.py
+++ main.py
@@ -46,7 +46,15 @@ def register():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    return redirect(url_for('index'))
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username,password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('index'))
 
 @app.route("/quote", methods = ['GET'])
 def get_quote():
