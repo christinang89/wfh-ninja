b2033028b4ba2ab8ec200e69c64845d70b541f7c
Christina Ng <christinang89@gmail.com>
2015-05-23 17:25:10 -0700

    added templates for login and register + add register code

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
 @app.route("/")
 def hello():
     return "Hello World!"
@@ -29,6 +31,22 @@ def hello():
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+ 
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return redirect(url_for('index'))
 
 @app.route("/quote", methods = ['GET'])
 def get_quote():
