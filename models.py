06f57edd0efef12f4c67f3c67b1ebbe7afb7647c
Christina Ng <christinang89@gmail.com>
2015-05-23 17:22:50 -0700

    update user model class with flask-login method

--- models.py
+++ models.py
@@ -64,3 +64,18 @@ class User(db.Model):
         self.password = password
         self.email = email
         self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
